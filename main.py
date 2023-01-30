29b2c77a4df9c67fdb6ecf629bfbb7af2b4c4ba3
katayaman <daisuke23bubu@gmail.com>
2023-01-31 06:22:34 +0900

    feat: svm

--- main.py
+++ main.py
@@ -1,12 +1,61 @@
 import matplotlib.pyplot as plt
 
-from sklearn import datasets, svm, metrics
+import numpy as np
+from sklearn import datasets, svm, metrics, decomposition
 from sklearn.model_selection import train_test_split
 
-digits = datasets.load_digits()
+# データセット
+faces = datasets.fetch_olivetti_faces()
+faces.data.shape
 
-_,axes = plt.subplots(nrows=1,ncols=4,figsize=(10,3))
-for ax, image, label in zip(axes,digits.images,digits.target):
-    ax.set_axis_off()
-    ax.imshow(image,cmap=plt.cm.gray_r,interpolation="nearest")
-    ax.set_title("Training: %i" % label)
+# データセット描画
+fig = plt.figure(figsize=(8, 6))
+for i in range(15):
+    ax = fig.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
+    ax.imshow(faces.images[i], cmap=plt.cm.bone)
+
+X_train, X_test, y_train, y_test = train_test_split(faces.data,
+        faces.target, random_state=0)
+
+print(X_train.shape, X_test.shape)
+
+# PCA
+pca = decomposition.PCA(n_components=150, whiten=True, svd_solver='randomized')
+pca.fit(X_train)
+
+# 平均を描画
+plt.imshow(pca.mean_.reshape(faces.images[0].shape), cmap=plt.cm.bone)
+
+print(pca.components_.shape)
+
+# 構成要素を描画
+fig = plt.figure(figsize=(16, 6))
+for i in range(30):
+    ax = fig.add_subplot(3, 10, i + 1, xticks=[], yticks=[])
+    ax.imshow(pca.components_[i].reshape(faces.images[0].shape), cmap=plt.cm.bone)
+
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+print(X_train_pca.shape)
+print(X_test_pca.shape)
+
+# SVM分類
+clf = svm.SVC(C=5., gamma=0.001)
+clf.fit(X_train_pca, y_train)
+
+# テストケースの実行
+fig = plt.figure(figsize=(8, 6))
+for i in range(15):
+    ax = fig.add_subplot(3, 5, i + 1, xticks=[], yticks=[])
+    ax.imshow(X_test[i].reshape(faces.images[0].shape),
+              cmap=plt.cm.bone)
+    y_pred = clf.predict(X_test_pca[i, np.newaxis])[0]
+    color = ('black' if y_pred == y_test[i] else 'red')
+    ax.set_title(faces.target[y_pred],
+                 fontsize='small', color=color)
+
+y_pred = clf.predict(X_test_pca)
+print(metrics.classification_report(y_test, y_pred))
+
+print(metrics.confusion_matrix(y_test, y_pred))
